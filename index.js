dc4fc3da552acc9dc134455c2e2a50944ae0cf43
Alan <jiglitchialan@gmail.com>
2025-11-18 09:58:23 +0200

    I have added some of my projects

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ const body = document.body;
 themeBtn.addEventListener('click', () => {
     body.classList.toggle('light-theme');
 });
+
+const projectsBtn = document.getElementById('projects-btn');
+const mainContent = document.getElementById('main-content');
+const projectsSection = document.getElementById('projects-section');
+
+projectsBtn.addEventListener('click', () => {
+    mainContent.style.display = 'none';
+    projectsSection.style.display = 'block';
+});
