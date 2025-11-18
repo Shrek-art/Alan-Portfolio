8161fc7a6ef696928f3babf2d2881b631ec36be3
Alan <jiglitchialan@gmail.com>
2025-11-18 10:16:30 +0200

    I added some of my projects

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// ============ THEME BUTTON ============
 const themeBtn = document.getElementById('themeBtn');
 const body = document.body;
 
@@ -5,6 +6,7 @@ themeBtn.addEventListener('click', () => {
     body.classList.toggle('light-theme');
 });
 
+// ============ PROJECTS BUTTON ============
 const projectsBtn = document.getElementById('projects-btn');
 const mainContent = document.getElementById('main-content');
 const projectsSection = document.getElementById('projects-section');
@@ -12,4 +14,37 @@ const projectsSection = document.getElementById('projects-section');
 projectsBtn.addEventListener('click', () => {
     mainContent.style.display = 'none';
     projectsSection.style.display = 'block';
+});
+
+// ============ PROJECTS TABS FILTER ============
+const tabs = document.querySelectorAll('.project-tab');
+const projects = document.querySelectorAll('.project');
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+
+        // Setăm tab-ul activ
+        tabs.forEach(t => t.classList.remove('active'));
+        tab.classList.add('active');
+
+        const category = tab.getAttribute('data-category');
+
+        // Arătăm doar proiectele din categoria selectată
+        projects.forEach(project => {
+            if (project.getAttribute('data-category') === category) {
+                project.style.display = 'block';
+            } else {
+                project.style.display = 'none';
+            }
+        });
+    });
+});
+
+const homeBtn = document.getElementById('home-btn');
+
+homeBtn.addEventListener('click', () => {
+    // Arată landing page-ul
+    mainContent.style.display = 'block';
+    // Ascunde secțiunea Projects
+    projectsSection.style.display = 'none';
 });
